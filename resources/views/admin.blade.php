bafaec1143c61f3368f9433656dad031b548764c
Mahmoud Hosny <7oodamarwaan31@gmail.com>
2025-10-17 15:48:27 +0300

    Initial commit by Mahmoud Hosny

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <p class="alert alert-success"> You are the Admin and you are being logged in </p>
+    </div>
+    @stop
